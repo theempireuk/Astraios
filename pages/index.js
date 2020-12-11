8cf709dac57aa45af40e4c361aab7781e917f665
Morgan Lee <morgan@Morgans-MacBook-Pro.local>
2020-12-11 19:27:28 +0000

    email ting

--- pages/index.js
+++ pages/index.js
@@ -130,7 +130,7 @@ export default function Home() {
                   { fixed: '@' },
                   { regexp: /^[\w]+$/, placeholder: 'my' },
                   { fixed: '.' },
-                  { regexp: /^[\w]+$/, placeholder: 'com' },
+                  { regexp: /^[\w.]+$/, placeholder: 'com' },
                 ]}
               />
             </FormField>
